ca4ef7d1dc350f48c480470c0b7cbd805af8c1c5
Verónica Soria <soriafontesveronica@gmail.com>
2022-12-10 12:00:50 -0300

    entrega

--- seed_data.py
+++ seed_data.py
@@ -1,7 +1,7 @@
 from ejemplo.models import Familiar
 
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+Familiar(fecha = "2020-01-01", nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
+Familiar(fecha = "2020-01-01", nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
+Familiar(fecha = "2020-01-01", nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
+Familiar(fecha = "2020-01-01", nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
 print("Se cargo con éxito los usuarios de pruebas")
